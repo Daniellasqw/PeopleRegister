7507f033ea97102341e3c3cc28257cd0d38dc808
Daniella Werneck <daniella.silqueiroz@gmail.com>
2025-01-21 01:48:19 -0300

    WIP

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,31 @@
 import { Stack } from 'expo-router';
-import { StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from '../src/redux/store';
-import InitialPage from './index';
 
 export default function Layout() {
 
 
- 
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-       <ReduxProvider store={store}>
-      <StatusBar barStyle={'light-content'} />
-      <Stack
-        screenOptions={{
-          headerStyle: { backgroundColor: '#7d7f9e' },
-          headerShown: false,
-          headerTintColor: '#fff',
-          headerTitleStyle: { fontWeight: 'bold' },
-        }}
-      >
-          <Stack.Screen name="index" />
-      </Stack>
-      </ReduxProvider>
-    </GestureHandlerRootView>
+    <SafeAreaView>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+
+        <ReduxProvider store={store}>
+          <StatusBar barStyle={'light-content'} />
+          <Stack
+            screenOptions={{
+              headerStyle: { backgroundColor: '#7d7f9e' },
+              headerShown: false,
+              headerTintColor: '#fff',
+              headerTitleStyle: { fontWeight: 'bold' },
+            }}
+          >
+            <Stack.Screen name="index" />
+          </Stack>
+        </ReduxProvider>
+      </GestureHandlerRootView>
+    </SafeAreaView>
   );
 }
